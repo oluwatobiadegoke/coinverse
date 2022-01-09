1a003cb53cf962400454081d91560619eacf1162
Oluwatobi Adegoke <adegoketobi5496@gmail.com>
2022-01-09 22:38:50 +0100

    Added nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import Router from "next/router";
+import nProgress from "nprogress";
+import type { AppProps } from "next/app";
+
 import "../styles/globals.css";
 import "antd/dist/antd.css";
-import type { AppProps } from "next/app";
+import "../styles/nprogress.css";
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
